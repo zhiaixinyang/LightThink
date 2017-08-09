ec081e16da06cc56671d1b1d239657e24bc7a4ed
zhiaixinyang <1024624013@qq.com>
2017-08-09 21:49:25 +0800

    尝试重新构建项目

---
+++ greatbook/src/main/java/com/example/greatbook/apis/MyBookApis.java
@@ -0,0 +1,10 @@
+package com.example.greatbook.apis;
+
+/**
+ * Created by MBENBEN on 2017/8/9.
+ */
+
+public class MyBookApis {
+    //http://www.ohonor.xyz/MyBook/querySectionNameByBookName?bookName=鬼故事
+    //http://www.ohonor.xyz/MyBook/queryBookContentByHref?bookSectionId=/gushihui/100geshijiejingdianbeihoudegushi/429564.html
+}
